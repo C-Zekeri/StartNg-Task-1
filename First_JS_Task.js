3d376813807b3aafc3475c92a7723da02780ab1a
C-Zekeri <55132738+C-Zekeri@users.noreply.github.com>
2020-03-25 10:32:28 +0100

    Add files via upload

---
+++ First_JS_Task.js
@@ -0,0 +1,9 @@
+var name = "Chenemi Zekeri"
+{ console.log(name); }
+var courses = new Array("HTML/CSS", "JavaScript", "UI/UX Design")
+for (i = 0; i <= 3; i++) {
+    console.log(courses[i] + "</br>");
+}
+for (i = 1; i <= 200; i += 2) {
+    console.log(i + "</br>");
+}
